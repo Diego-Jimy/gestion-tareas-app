dc6bf44ca87f601a18076b1a9ad394a4c8a6a3fc
Gato-Tofu <gatotofu2906@gmail.com>
2026-05-08 14:45:03 -0500

    Agregado la función modelo en JS

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const STORAGE_KEY = 'taskflow';
- 
-  let tasks = [];
-  let filter = 'todas';
-  let searchQuery = '';
-  let isDark = false;
- 
-// ─ PERSISTENCIA ─
-  function save() {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
-  }
- 
-  function load() {
-    try {
-      const raw = localStorage.getItem(STORAGE_KEY);
-      tasks = raw ? JSON.parse(raw) : [];
-    } catch { tasks = []; }
-  }
- 
 // ─ MODELO ─
   function createTask(name, priority) {
     return {

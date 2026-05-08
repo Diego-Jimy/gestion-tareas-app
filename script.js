214ecf3bcf713287bbc5d471be38bbac0d3e5c3c
Gato-Tofu <gatotofu2906@gmail.com>
2026-05-08 15:05:11 -0500

    Agregado la funcion de Persistencia en JS

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const STORAGE_KEY = 'taskflow';
- 
-  let tasks = [];
-  let filter = 'todas';
-  let searchQuery = '';
-  let isDark = false;
- 
+
+let tasks = [];
+let filter = 'todas';
+let searchQuery = '';
+let isDark = false;
+
 // ─ PERSISTENCIA ─
-  function save() {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
-  }
- 
-  function load() {
-    try {
-      const raw = localStorage.getItem(STORAGE_KEY);
-      tasks = raw ? JSON.parse(raw) : [];
-    } catch { tasks = []; }
-  }
- 
+function save() {
+localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+}
+
+function load() {
+try {
+    const raw = localStorage.getItem(STORAGE_KEY);
+    tasks = raw ? JSON.parse(raw) : [];
+} catch { tasks = []; }
+}
